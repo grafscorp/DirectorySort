ca34da2c46ead1e0d783e371ff3cbec7fba2e8d3
Grafscorp <golyandringraf@gmail.com>
2025-04-25 17:12:15 +0300

    Update DirectorySort

--- DirectorySort/include/DirectorySort.hpp
+++ DirectorySort/include/DirectorySort.hpp
@@ -3,7 +3,18 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <DirectoryTypeConfig.hpp>
 
+
+#pragma region ErrorCodes 
+
+#define NOT_DIRECTORY 1010
+
+#pragma endregion ErrorCodes
+
+#define NAME_DEFAULT_CONFIG "default_config.json"
+#define NAME_LOCAL_CONFIG ".ds_config.json"
 /*
 Main Class for sorting directories
 
@@ -15,13 +26,13 @@ public:
     ~DirectorySort();
 
 public:
-    void sort_directory();
+    void sort_directory(const std::string path);
 
 private:
     void create_default_architecture_directory();
     void get_default_archirecture_directory();
     void create_local_architecture_directory();
-    void get_local_architecture_diretory();
+    void get_local_architecture_diretory(const std::filesystem::path& dir);
 
 
 
--- DirectorySort/src/DirectorySort.cpp
+++ DirectorySort/src/DirectorySort.cpp
@@ -8,8 +8,19 @@ DirectorySort::~DirectorySort()
 {
 }
 
-void DirectorySort::sort_directory() {
+void DirectorySort::sort_directory(const std::string path) {
     
+    std::filesystem::path directory(path);
+
+
+    if (! std::filesystem::is_directory(directory))
+    {
+        throw NOT_DIRECTORY;
+        return; 
+    }
+
+
+
 }
 
 void DirectorySort::create_default_architecture_directory() {
@@ -20,5 +31,18 @@ void DirectorySort::create_default_architecture_directory() {
 }
 
 void DirectorySort::get_default_archirecture_directory() {
+
+}
+
+void DirectorySort::get_local_architecture_diretory(const std::filesystem::path& dir) {
+    
+    for(const auto& entry : std::filesystem::directory_iterator(dir))
+    {
+        if (entry.path().filename() == NAME_LOCAL_CONFIG) 
+        {
+            DirectoryTypeConfig localConfig;
+            
+        }
+    }
 
 }
