4fde54cb15e64abd334253a0a88d5b5bc0eb2eb4
Grafscorp <golyandringraf@gmail.com>
2025-04-25 17:12:50 +0300

    Update DirectoryTypeConfig
    add constructor and method is_empty

--- DirectorySort/include/DirectoryTypeConfig.hpp
+++ DirectorySort/include/DirectoryTypeConfig.hpp
@@ -8,9 +8,21 @@
 struct DirectoryTypeConfig
 {
 public:
+    DirectoryTypeConfig()
+    {
+
+    }
+    explicit  DirectoryTypeConfig(const std::string name, const std::string directory_name, const std::vector<std::string>formats):
+    name(name), directory_name(directory_name), formats(formats)
+    {
+
+    }
+
+
     std::string name;
     std::string directory_name;
     std::vector<std::string> formats;
+    bool is_empty();
 };
 
 
--- DirectorySort/src/DirectoryTypeConfig.cpp
+++ DirectorySort/src/DirectoryTypeConfig.cpp
@@ -1,1 +1,14 @@
 #include <DirectoryTypeConfig.hpp>
+
+bool DirectoryTypeConfig::is_empty()
+{
+    //TODO LOGER
+    if (name.empty() &&
+        directory_name.empty() &&
+        formats.empty()
+        )
+    {
+        return true;
+    }
+    return false;
+}
