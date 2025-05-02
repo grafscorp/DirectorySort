0b00ba8c6980d7d4b9ad397fefdb82112236fba5
Grafscorp <golyandringraf@gmail.com>
2025-05-02 15:59:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,25 @@
 
 int main(int argc, char const *argv[])
 {
+
+    if(argc != 2)
+    {
+        std::cerr << "Error\nUse: program <Path for sort>\n";
+        return 1;
+    }
+
+
     DirectorySort ds;
+    try
+    {
+        ds.sort_directory(argv[1]);
+    }
+    catch(const int e)
+    {
+        std::cerr << "Check is directory valid\nCode error : " << e << '\n';
+    }
     
-    ds.sort_directory(argv[1]);
+    
     //ds.create_local_architecture_directory("E:/test");
     return 0;
 }
